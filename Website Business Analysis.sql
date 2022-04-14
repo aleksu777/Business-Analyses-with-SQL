e0c48938dfe6fd5ec0048b801383d8b1e62d10aa
Aleks Udev <aleks_udev@hotmail.com>
2022-04-14 16:19:56 -0700

    SQL
     Business Analyses for a Website business

---
+++ Website Business Analysis.sql
@@ -0,0 +1,142 @@
+
+-- Pulling search sessions and orders rlationship in a time window to illustrate campaign results.
+
+-- 
+
+SELECT
+    ROUND(COUNT(DISTINCT orders.order_id)/COUNT(DISTINCT website_sessions.website_session_id)*100, 2) AS conv_rate,
+	YEAR(website_sessions.created_at) AS year, 
+    MONTH(website_sessions.created_at) AS month, 
+    COUNT(DISTINCT website_sessions.website_session_id) AS sessions, 
+    COUNT(DISTINCT orders.order_id) AS orders   
+FROM website_sessions
+	LEFT JOIN orders 
+		ON orders.website_session_id = website_sessions.website_session_id
+WHERE website_sessions.created_at < '2012-11-27'
+	AND website_sessions.utm_source = 'gsearch'
+GROUP BY year,month
+ORDER BY conv_rate DESC;
+
+
+
+-- Pulling monthly sessions and orders by device type -- desktop and mobile. 
+
+
+SELECT
+    
+	YEAR(website_sessions.created_at) AS year, 
+    MONTH(website_sessions.created_at) AS month, 
+    COUNT(DISTINCT CASE WHEN device_type = 'desktop' THEN website_sessions.website_session_id ELSE NULL END) AS desktop_sessions, 
+    COUNT(DISTINCT CASE WHEN device_type = 'desktop' THEN orders.order_id ELSE NULL END) AS desktop_orders,
+    COUNT(DISTINCT CASE WHEN device_type = 'mobile' THEN website_sessions.website_session_id ELSE NULL END) AS mobile_sessions, 
+    COUNT(DISTINCT CASE WHEN device_type = 'mobile' THEN orders.order_id ELSE NULL END) AS mobile_orders
+    
+FROM website_sessions
+	LEFT JOIN orders 
+		ON orders.website_session_id = website_sessions.website_session_id
+WHERE website_sessions.created_at < '2012-11-27'
+	AND website_sessions.utm_source = 'gsearch'
+    AND website_sessions.utm_campaign = 'nonbrand'
+GROUP BY year, month
+ORDER BY month;
+
+
+-- We can separated paid and organic sessions.
+
+
+SELECT
+	YEAR(website_sessions.created_at) AS year, 
+    MONTH(website_sessions.created_at) AS month, 
+    COUNT(DISTINCT CASE WHEN utm_source = 'gsearch' THEN website_sessions.website_session_id ELSE NULL END) AS Paid_sessions,
+    COUNT(DISTINCT CASE WHEN utm_source IS NULL AND http_referer IS NOT NULL THEN website_sessions.website_session_id ELSE NULL END) AS Organic_sessions
+FROM website_sessions
+	LEFT JOIN orders 
+		ON orders.website_session_id = website_sessions.website_session_id
+WHERE website_sessions.created_at < '2012-11-27'
+GROUP BY year, month
+ORDER BY Paid_sessions DESC;
+
+
+
+-- Estimating revnue using temp table
+
+
+
+SELECT
+	MIN(website_pageview_id) AS first_test_pv
+FROM website_pageviews
+WHERE pageview_url = '/lander-1';
+
+
+-- for this step, we'll find the first pageview id 
+
+CREATE TEMPORARY TABLE first_test_pageviews
+SELECT
+	website_pageviews.website_session_id, 
+    MIN(website_pageviews.website_pageview_id) AS min_pageview_id
+FROM website_pageviews 
+	INNER JOIN website_sessions 
+		ON website_sessions.website_session_id = website_pageviews.website_session_id
+		AND website_sessions.created_at < '2012-07-28' -- prescribed by the assignment
+		AND website_pageviews.website_pageview_id >= 23504 -- first page_view
+        AND utm_source = 'gsearch'
+        AND utm_campaign = 'nonbrand'
+GROUP BY 
+	website_pageviews.website_session_id;
+    
+    
+-- next, we'll bring in the landing page to each session 
+
+
+CREATE TEMPORARY TABLE nonbrand_test_sessions_w_landing_pages
+SELECT 
+	first_test_pageviews.website_session_id, 
+    website_pageviews.pageview_url AS landing_page
+FROM first_test_pageviews
+	LEFT JOIN website_pageviews 
+		ON website_pageviews.website_pageview_id = first_test_pageviews.min_pageview_id
+WHERE website_pageviews.pageview_url IN ('/home','/lander-1'); 
+
+
+
+-- to find the difference between conversion rates 
+
+SELECT
+	landing_page, 
+    COUNT(DISTINCT website_session_id) AS sessions, 
+    COUNT(DISTINCT order_id) AS orders,
+    COUNT(DISTINCT order_id)/COUNT(DISTINCT website_session_id) AS conv_rate
+FROM nonbrand_test_sessions_w_orders
+GROUP BY 1; 
+
+
+
+SELECT 
+	MAX(website_sessions.website_session_id) AS most_recent_gsearch_nonbrand_home_pageview 
+FROM website_sessions 
+	LEFT JOIN website_pageviews 
+		ON website_pageviews.website_session_id = website_sessions.website_session_id
+WHERE utm_source = 'gsearch'
+	AND utm_campaign = 'nonbrand'
+    AND pageview_url = '/home'
+    AND website_sessions.created_at < '2012-11-27'
+;
+
+
+-- max website_session_id = 17145
+
+
+SELECT 
+	COUNT(website_session_id) AS sessions_since_test
+FROM website_sessions
+WHERE created_at < '2012-11-27'
+	AND website_session_id > 17145 -- last /home session
+	AND utm_source = 'gsearch'
+	AND utm_campaign = 'nonbrand'
+;
+
+-- 22,972 website sessions since the test
+
+-- roughly 4 months, so roughly 50 extra orders per month.
+
+
